a00aec71e5a8b8bcbffc62ca24ac94112f45b9b9
dskst <grandevivo9-dskst@yahoo.co.jp>
2019-02-22 00:44:31 +0900

    Use Triangulation to Dollar class

--- src/main/kotlin/dskst/tdd/money/Dollar.kt
+++ src/main/kotlin/dskst/tdd/money/Dollar.kt
@@ -4,7 +4,7 @@ class Dollar(amount: Int) {
 
     var amount = amount
 
-    fun times(multiplier: Int) {
-        amount *= multiplier
+    fun times(multiplier: Int): Dollar {
+        return Dollar(amount * multiplier)
     }
 }
