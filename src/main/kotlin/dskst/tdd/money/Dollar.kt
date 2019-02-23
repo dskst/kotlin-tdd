4d63e43d7e091e63e7059ed9c7d01b372ef6d63d
dskst <grandevivo9-dskst@yahoo.co.jp>
2019-02-23 14:55:45 +0900

    Change access to field to private

--- src/main/kotlin/dskst/tdd/money/Dollar.kt
+++ src/main/kotlin/dskst/tdd/money/Dollar.kt
@@ -2,7 +2,7 @@ package dskst.tdd.money
 
 class Dollar(amount: Int) {
 
-    var amount = amount
+    private var amount = amount
 
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
